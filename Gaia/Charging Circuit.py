7bc6f1edcbf98b913d8a3b3d3b98001cec016bdd
Crazysodaman <77500555+Crazysodaman@users.noreply.github.com>
2021-02-10 18:04:54 -0600

    Update Charging Circuit.py

--- Gaia/Charging Circuit.py
+++ Gaia/Charging Circuit.py
@@ -1,6 +1,15 @@
 import time
 import RPi.GPIO as GPIO
+import ssc32 as s32
+
+
+CRelay= 23
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT[intial=0])
+GPIO.setmode(GPIO.BCM) #set pins to GPIO# like GPIO 4 is #7 on board
+GPIO.setup(CRelay, GPIO.OUT)
+GPIO.output(CRelay, GPIO.LOW)
+
+if cfg.charging == 1
+  s32.32cl
+  GPIO.output(CRelay, GPIO.HIGH)
