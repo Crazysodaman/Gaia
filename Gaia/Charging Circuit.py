624cd657a9bea7ab15e88df37be6808ce66d0889
Crazysodaman <77500555+Crazysodaman@users.noreply.github.com>
2021-02-11 10:16:52 -0600

    Update Charging Circuit.py
    Need to Finish loop

--- Gaia/Charging Circuit.py
+++ Gaia/Charging Circuit.py
@@ -11,6 +11,8 @@ GPIO.setmode(GPIO.BCM) #set pins to GPIO# like GPIO 4 is #7 on board
 GPIO.setup(CRelay, GPIO.OUT)
 GPIO.output(CRelay, GPIO.LOW)
 
-if cfg.charging == 1:
-  s32.ssc.close
+def relayon()
+loop(
   GPIO.output(CRelay, GPIO.HIGH)
+#if (insert voltage here) >= 100%
+GPIO.output(CRelay, GPIO.LOW))
