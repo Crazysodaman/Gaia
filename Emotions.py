66792c060c8cade07c8da8af5d34ac19350407c9
Crazysodaman <77500555+Crazysodaman@users.noreply.github.com>
2021-09-19 12:21:05 -0500

    Update Emotions.py

--- Emotions.py
+++ Emotions.py
@@ -1,4 +1,5 @@
 import random
+import filehandler as fh
 
 class Emotions:
 
@@ -12,17 +13,45 @@ class Emotions:
         self.mcm = 50
         self.mhs = 50
 
+    def setneutralsave(self):
+        self.mgm = 50
+        self.mcm = 50
+        self.mhs = 50
+        fh.writedataa(self.mhs, "Emotions", "mhs")
+        fh.writedataa(self.mgm, "Emotions", "mgm")
+        fh.writedataa(self.mcm, "Emotions", "mcm")
+
     def readhappysad(self):
         return self.mhs
 
     def readcalmmad(self):
         return self.mcm
 
-    def readrgoodmischief(self):
+    def readgoodmischief(self):
         return self.mgm
+
+    def filehappysad(self):
+
+        return fh.readdataa("Emotions","mhs")
+
+    def filecalmmad(self):
+        return fh.readdataa("Emotions","mcm")
+
+    def filegoodmischief(self):
+        return fh.readdataa("Emotions","mgm")
+
+    def savehappysad(self):
+        return fh.writedataa(self.mhs, "Emotions", "mhs")
+
+    def savecalmmad(self):
+        return fh.writedataa(self.mcm, "Emotions", "mcm")
+
+    def savegoodmis(self):
+        return fh.writedataa(self.mgm, "Emotions", "mgm")
 
     def randommoodhappysad(self):
         self.mhs = random.randrange(0, 100, 5)
+        fh.writedataa(self.mhs, "Emotions", "mhs")
         return self.mhs
 
     def randommoodcalmmad(self):
@@ -35,11 +64,18 @@ class Emotions:
 
     def makehappysad(self, i):
         self.mhs = self.mhs + i
+        fh.writedataa(self.mhs, "Emotions", "mhs")
         return self.mhs
 
     def makecalmmad(self, i):
         self.mcm = self.mcm + i
+        fh.writedataa(self.mcm, "Emotions", "mcm")
         return self.mcm
+
+    def makegoodmis(self, i):
+        self.mgm = self.mgm + i
+        fh.writedataa(self.mgm, "Emotions", "mgm")
+        return self.mgm
 
 
 class HappySad(Emotions):
@@ -58,3 +94,7 @@ class GoodMischief(Emotions):
 
     def __init__(self):
         super(GoodMischief, self).__init__()
+
+if __name__ == '__main__':
+    Emote=Emotions()
+    Emote.setneutralsave()
